d33bde430b11e363dce6a9e696d33473abde2b4e
Jon Suderman <suderman@gmail.com>
2015-05-12 16:49:51 -0600

    fixes

--- src/save.js
+++ src/save.js
@@ -1,39 +1,82 @@
 var _ = require('lodash'),
     fs = require('fs'),
+    colors = require('colors'),
     request = require('sync-request');
+require('shelljs/global');
+
+// Regular express for blank lines
+var blankLines = new RegExp(/^\s*[\r\n]/gm);
+
+var saving = '=> Saving '.green,
+    existing = '=> Existing '.yellow,
+    overwriting = '=> Overwriting '.red,
+    reverting = '=> Reverting '.yellow;
+
+// Save or download a file
+function save(path, data, options) {
+
+  // Check if the data looks like a URL
+  if ((_.startsWith(data, 'http://')) || (_.startsWith(data, 'https://'))) {
+
+    // If so, download the URL and set data
+    var url = data;
+    data = request('GET', url).getBody('utf8');
+
+  // Remove blank lines
+  } else {
+    data = data.replace(blankLines, '');
+  } 
+
+  // Write the data to the path
+  fs.writeFileSync(path, data);
+
+  // Change ownership of file
+  if (options.user) {
+    exec(`chown ${options.user} ${path}`, { silent: true });
+    exec(`chgrp ${options.user} ${path}`, { silent: true });
+  }
+
+  // If file is shell script, make executable
+  if (_.endsWith(path, '.sh')) {
+    fs.chmodSync(path, '755');
+  }
+
+  // Protect key, pem and htpasswd files
+  if ((_.endsWith(path, '.key')) || (_.endsWith(path, '.pem')) || (_.endsWith(path, 'htpasswd'))) {
+    fs.chmodSync(path, '600');
+  }
+}
 
 // Save data (or data from URL) to path
-module.exports = function(path, data) {
+module.exports = function(path, data, options = { force: false, user: process.env.USER }) {
 
   // If data empty, exit
   if (!_.trim(data)) return;
 
   // Don't do anything if the file already exists
   try {
-    fs.readFileSync(path, 'utf8');
+    var existingData = fs.readFileSync(path, 'utf8');
+    console.log(existing + path);
+
+    // If file exists, only proceed to overwrite if force enabled
+    if (options.force) { 
+
+      // Delete the old file and write the new data
+      console.log(overwriting + path);
+      fs.unlinkSync(path);
+      save(path, data, options); 
+
+      // Ensure new data exists, otherwise revert to old data
+      if (fs.readFileSync(path, 'utf8') == '') {
+        console.log(reverting + path);
+        save(path, existingData, options); 
+      }
+    }
 
   // Otherwise, go for it
   } catch (e) {
-
-    // Check if the data looks like a URL
-    if ((_.startsWith(data, 'http://')) || (_.startsWith(data, 'https://'))) {
-
-      // If so, download the URL and set data
-      var url = data;
-      data = request('GET', url).getBody('utf8');
-    } 
-
-    // Write the data to the path
-    fs.writeFileSync(path, data);
-
-    // If file is shell script, make executable
-    if (_.endsWith(path, '.sh')) {
-      fs.chmodSync(path, '755');
-    }
-
-    // Protect key and pem files
-    if ((_.endsWith(path, '.key')) || (_.endsWith(path, '.pem'))) {
-      fs.chmodSync(path, '600');
-    }
+    console.log(saving + path);
+    save(path, data, options);
   }
+
 }
--- src/templates.js
+++ src/templates.js
@@ -4,11 +4,13 @@ var fs = require('fs'),
     colors = require('colors');
 
 var dir = path.resolve(path.join(__dirname, '../templates'));
-var templates = {};
+var templates = { main: {} };
 
 try {
 
+  templates.main.nginx = hbs.compile(fs.readFileSync(dir + '/main.nginx.conf.hbs', 'utf8'));
   templates.nginx = hbs.compile(fs.readFileSync(dir + '/nginx.conf.hbs', 'utf8'));
+  templates.main.dnsmasq = hbs.compile(fs.readFileSync(dir + '/main.dnsmasq.conf.hbs', 'utf8'));
   templates.dnsmasq = hbs.compile(fs.readFileSync(dir + '/dnsmasq.conf.hbs', 'utf8'));
   templates.dnsimple = hbs.compile(fs.readFileSync(dir + '/dnsimple.sh.hbs', 'utf8'));
 
